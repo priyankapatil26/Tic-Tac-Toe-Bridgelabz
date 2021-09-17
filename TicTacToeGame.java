0ed33719fcb895389d2d6a88946c518afa6f5452
Priyanka Patil1 <priyanka.patil1@morningstar.com>
2021-09-17 17:38:52 +0530

    Choose X or O

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,15 +1,52 @@
-package JAVAFIRST;
+package com.bridgelabz.tictactoe;
+
+import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToeGame {
-	char[] board = new char[10];
+
+	static char[] board = new char[10];
+	static String turn;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("This is Tic-Tac-Toe Game");
+		initBoard();
+		int player = Toss();
+		// player checked 1-user, 0-computer
+		if (player == 1) {
+			System.out.println("User plays first.");
+			chooseLetter();
+		}
+		if (player == 0) {
+			System.out.println("Computer plays first.");
+			chooseLetter();
+		}
 	}
-		public void InitBoard() {
-		for(int i=1;i<=9;i++) {
-			board[i]=0;
+
+	public static void initBoard() {
+		for (int i = 1; i <= 9; i++) {
+			board[i] = ' ';
 		}
+	}
+
+	// Function to choose letter X or O
+	public static String chooseLetter() {
+		System.out.println("Choose either X or O: ");
+		Scanner letter = new Scanner(System.in);
+		String inputLetter = letter.next();
+		if (inputLetter.equals("X")) {
+			turn = "X";
+		} else {
+			turn = "O";
 		}
+		return turn;
+	}
+
+	// Function to generate random player
+	public static int Toss() {
+		System.out.println("Toss to see who plays first!");
+		Random random = new Random();
+		return random.nextInt(2);
+	}
+
 }
